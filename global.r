30a803e3438daa4a1e2b52bd073483c9fccdca37
Gregory Weber <gpweber3@gmail.com>
2020-08-06 23:34:08 -0400

    Wrote message explaining code
    Added the dataframe for artist counts.
    
    server.r still mostly empty

--- global.r
+++ global.r
@@ -3,18 +3,43 @@ library(tidyverse)
 library(shinydashboard)
 library(RColorBrewer)
 library(tidyr)
+#csv of RockHall playlists from spotify
+#obtained with spotipy using python
 famer = read.csv('famer.csv')
 
-
+# create function to remove first 2 and last 2 characters of a string
 fun = function(x){
   str_sub(x, 3,-3)
 }
 
+#remove brackets and extra quotes from beginning and end of artists listings
 famer$track_artists = sapply(famer$track_artists, fun )
+
+#split artists into separate cells (lots of NAs are produced)
 famer = separate(famer, track_artists, sep = "', '", c('first_artist','second_artist','third_artist','fourth_artist', 
           'fifth_artist', "sixth', 'seventh','eighth_artist(s)'"), extra = 'merge')
 
+#use the number of NA's to determine the number of artists in a row 
 famer$number_of_artists = apply(famer, 1, ( function(x) sum(is.na(x))*-1 +6) )
 
+#There is one track with more than 6 artists
+#manual change the number_of_artists count for that track
 famer$number_of_artists[2883] = 8
 
+############create dataframe with artist counts#####################
+#combine the 5 artist columns into one vector
+x = unlist(famer[4:8])
+
+#remove the NA's
+x1 = x[!is.na(x)]
+
+#Turn into a dataframe
+x2 = as.data.frame(x1)
+
+#add the 5 extra artists from the sixth spillover column
+x3 = x2 %>% 
+  rbind('Yo Gotti', 'Quavo', 'Desiigner', 'Paul Shaffer', 'Metallica')
+
+#create total_tracks dataframe with tallies for number of tracks per artist
+total_tracks = x3 %>% 
+  count(x1, sort = TRUE, name = "Total_Artist_Tracks")
