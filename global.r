06d591502039d2766be24ff27d184ef49e901fcf
Gregory Weber <gpweber3@gmail.com>
2020-08-06 20:52:59 -0400

    Extra line deleted.

--- global.r
+++ global.r
@@ -12,9 +12,9 @@ fun = function(x){
 
 famer$track_artists = sapply(famer$track_artists, fun )
 famer = separate(famer, track_artists, sep = "', '", c('first_artist','second_artist','third_artist','fourth_artist', 
-          'fifth_artist', "sixth', 'seventh', ', 'eighth_artist(s)"), extra = 'merge')
+          'fifth_artist', "sixth', 'seventh','eighth_artist(s)'"), extra = 'merge')
 
-famer$na_count <- apply(famer, 1, function(x) sum(is.na(x)))
-famer %>% 
-  separate(c("first_artist":'fifth_artist'), count = NA, append = TRUE, var = 'number_of_artists')
-famer
+famer$number_of_artists = apply(famer, 1, ( function(x) sum(is.na(x))*-1 +6) )
+
+famer$number_of_artists[2883] = 8
+
