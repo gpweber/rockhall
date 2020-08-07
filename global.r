c0263841aeb590efee45c9f07e7ba57a3218edd9
Gregory Weber <gpweber3@gmail.com>
2020-08-06 20:49:39 -0400

    Fixed global back to fully edited famer.csv. With: - number_of_artists column name changed from drop_na & calculation changed so that it's correct - famer$number_of_artists[2883] changed to 8 artists

--- global.r
+++ global.r
@@ -4,12 +4,13 @@ library(shinydashboard)
 library(RColorBrewer)
 library(tidyr)
 famer = read.csv('famer.csv')
-dim(famer)
+
 
 fun = function(x){
   str_sub(x, 3,-3)
 }
-head(famer$track_artists) = sapply(famer$track_artists, fun )
+
+famer$track_artists = sapply(famer$track_artists, fun )
 famer = separate(famer, track_artists, sep = "', '", c('first_artist','second_artist','third_artist','fourth_artist', 
           'fifth_artist', "sixth', 'seventh', ', 'eighth_artist(s)"), extra = 'merge')
 
